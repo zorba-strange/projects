aae3fed41760e0b836afaba6b79681d0f3c16c6e
winston mclean <zorba@winstons-MacBook-Pro.local>
2016-06-07 13:31:37 -0400

    added id to input and jquery keypress enter(not working right)

--- script.js
+++ script.js
@@ -3,4 +3,10 @@ $(document).ready(function() {
     var toAdd = $('input[name=toDoList]').val();
     $('ol').append('<li class="item">' + toAdd + '</li>');
   });
+  $('#input').keypress(function(e) {
+    if(e.which == 13){
+      var toAdd = $('input[name=toDoList]').val();
+      $('ol').append('<li class="item">' + toAdd + '</li>');
+    }
+  });
 });
