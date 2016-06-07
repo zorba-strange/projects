10a71832f25aa395f5e6a8e4a0fa80d0e806d4a1
winston mclean <zorba@winstons-MacBook-Pro.local>
2016-06-07 15:13:25 -0400

    got keypress enter working. Cleared input field after .click and .keypress

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
+
+//jQuery
 $(document).ready(function() {
+  //when button is clicked add text to list
   $('button').click(function() {
+    //text from input to add to lsit
     var toAdd = $('input[name=toDoList]').val();
+    $('input[name=toDoList]').val('');
     $('ol').append('<li class="item">' + toAdd + '</li>');
   });
+  //when enter key is press text is entered into list
   $('#input').keypress(function(e) {
-    if(e.which == 13){
+    if(e.keyCode == 13) {
       var toAdd = $('input[name=toDoList]').val();
       $('ol').append('<li class="item">' + toAdd + '</li>');
-    }
+      event.currentTarget.value = "";
+      return false; //stops the default event triggering so list doesnt clear
+    };
   });
 });
